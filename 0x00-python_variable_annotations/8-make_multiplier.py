2064f526ba657b655f0c4a025f161edd780e9871
Manuel254 <Immanuelkituku@gmail.com>
2023-07-08 22:34:00 +0300

    A function that returns a function that multiplies the function argument

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+"""Function that takes in a float as an argument
+and returns a function that multiplies a float by multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies by the argument"""
+    return lambda x: x * multiplier
