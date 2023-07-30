f3e555f8615440f28776fe1694d083aadf6b9a4f
Manuel254 <immanuelkituku@gmail.com>
2023-07-30 18:23:17 +0000

    Add test for memoized function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 import utils
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from unittest.mock import Mock, patch
 
 
@@ -51,3 +51,25 @@ class TestGetJson(unittest.TestCase):
 
             mock_requests.get.assert_called_once_with(url)
             self.assertEqual(payload, result)
+
+
+class TestMemoize(unittest.TestCase):
+    """Contains a memoized method"""
+    def test_memoize(self):
+        """Test memoize method"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            mock_method.return_value = 42
+
+            obj = TestClass()
+            self.assertEqual(obj.a_property, 42)
+            self.assertEqual(obj.a_property, 42)
+            mock_method.assert_called_once()
