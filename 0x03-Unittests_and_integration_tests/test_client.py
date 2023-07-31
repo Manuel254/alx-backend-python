e0c5d52f3946d76cbc1eb70dfd63f69043b16da3
Manuel254 <immanuelkituku@gmail.com>
2023-07-31 08:11:50 +0000

    Add test for has_license method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -30,3 +30,46 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(
                     GithubOrgClient('google')._public_repos_url, url
             )
+    """
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        #Test public_repos method
+        payload = {
+            "repos_url": "https://api.github.com/orgs/google/repos",
+            "repos": [
+                {
+                    "id": 7697149,
+                    "node_id": "MDEwOlJlcG9zaXRvcnk3Njk3MTQ5",
+                    "name": "episodes.dart",
+                    "full_name": "google/episodes.dart"
+                },
+                {
+                    "id": 7776515,
+                    "node_id": "MDEwOlJlcG9zaXRvcnk3Nzc2NTE1",
+                    "name": "cpp-netlib",
+                    "full_name": "google/cpp-netlib"
+                }
+            ]
+        }
+
+        mock_get_json.return_value = payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = payload['repos_url']
+            self.assertEqual(GithubOrgClient('google').public_repos(), 
+                    [
+                        "episodes.dart",
+                        "cpp-netlib"
+                    ])
+    """
+    @parameterized.expand([
+        ({"key": "my_license"}, "my_license", True),
+        ({"key": "other_license"}, "my_license", False)
+    ])
+    def test_has_license(self, my_dict, license, expected):
+        """Test if repo has license"""
+        with patch('client.GithubOrgClient.has_license') as mock_has_license:
+            org = GithubOrgClient('google')
+            mock_has_license.return_value = expected
+            self.assertEqual(org.has_license(my_dict, license), expected)
